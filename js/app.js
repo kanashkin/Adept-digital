20571063b4ffb5703991292ae578778003bdcc72
Alexander <kanashkin_ad@mail.ru>
2024-03-26 01:12:32 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,40 @@ const reviewsSwiper = new Swiper('.reviews-swiper', {
     freeMode: true, // Включаем свободный режим
     freeModeSticky: true, // Включаем закрепление в свободном режиме
 })
+
+function blockHeight() {
+    const block = document.querySelector('.reviews__info')
+    const blockHeight = block.clientHeight
+
+    block.style.height = blockHeight - 20 + 'px'
+}
+
+blockHeight()
+
+function videoPlayer() {
+    const video = document.querySelector('.video-player')
+    const seekBar = document.querySelector('.video-input')
+    const playBtn = document.querySelector('.video-play')
+
+    video.addEventListener('timeupdate', function() {
+        let value = (video.currentTime / video.duration) * 100
+        console.log(value);
+        seekBar.value = value
+    })
+
+    seekBar.addEventListener('change', function() {
+        let time = video.duration * (seekBar.value / 100)
+        video.currentTime = time
+    })
+
+    playBtn.addEventListener('click', function() {
+        if(video.paused) {
+            video.play()
+            video.classList.add('active')
+        } else {
+            video.pause()
+        }
+    })
+}
+
+videoPlayer()
